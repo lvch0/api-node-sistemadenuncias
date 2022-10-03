0a42c78d4ad04169ebb0a37f8aec15996dae8e00
lvch0 <100215416+lvch0@users.noreply.github.com>
2022-10-02 23:48:55 -0300

    CRUD módulo usuarios finalizado

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -58,33 +58,65 @@ const prisma = new PrismaClient()
 const usersCtrl = {}
 
 usersCtrl.getUsers = async (req, res) => {
-    const result = await prisma.usuario.findMany()
-    res.json(result)
+    try {
+        const result = await prisma.usuario.findMany()
+        res.json(result)
+    } catch (error) {
+        res.send(error)
+    }
 }
 
-usersCtrl.getUser = (req, res) => {
-    const { id } = req.params
-
+usersCtrl.getUser = async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await prisma.usuario.findUnique({
+            where: {
+                IdUsuario: Number(id)
+            },
+        })
+        res.json(result)
+    } catch (error) {
+        res.send(error)
+    }
 }
 
 usersCtrl.createUser = async (req, res) => {
-    const { nombreUsuario, contrasenia, rol } = req.body
-    const result = await prisma.usuario.create({
-        data: {
-            nombreUsuario,
-            contrasenia,
-            rol
-        }
-    })
-    res.json(result)
+    try {
+        const { nombreUsuario, contrasenia, rol } = req.body
+        const result = await prisma.usuario.create({
+            data: {
+                nombreUsuario,
+                contrasenia,
+                rol
+            }
+        })
+        res.json(result)
+    } catch (error) {
+        res.send(error)
+    }
 }
 
-usersCtrl.updateUser = (req, res) => {
-
+usersCtrl.updateUser = async (req, res) => {
+    try {
+        const { id } = req.params
+        const result = await prisma.usuario.update({
+            where: { 
+                IdUsuario: Number(id)
+            },
+            data: req.body
+        })
+        res.json(result)
+    } catch (error) {
+        res.send(error)
+    }
 }
 
 usersCtrl.deleteUser = (req, res) => {
+    try {
 
+    } catch (error) {
+
+    }
 }
 
 module.exports = usersCtrl
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -4,13 +4,9 @@ const router = Router()
 const usersCtrl = require("../controllers/usuarios.controller")
 
 router.get("/", usersCtrl.getUsers)
-
 router.get("/:id", usersCtrl.getUser)
-
 router.post("/", usersCtrl.createUser)
-
 router.put("/:id", usersCtrl.updateUser)
-
 router.delete("/:id", usersCtrl.deleteUser)
 
 module.exports = router
