b338ef58f79dcec60c4d656c5f9ea31adb97113b
lvch0 <100215416+lvch0@users.noreply.github.com>
2022-10-03 16:44:15 -0300

    mbackend usuario funcional

---
+++ src/middleware/createUserCheck.js
@@ -0,0 +1,12 @@
+const emptyName(j) => {
+    if (!j) {
+        throw new Error("Se debe ingresar un nombre para la creación de usuario.")
+    }
+}
+
+const nameUpperCase(j) => {
+    if (/^\s+$/.test(f)) {
+        throw new Error("No es posible ingresar espacios en blanco.")
+    }
+}
+
