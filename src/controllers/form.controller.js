69104ca5f909b765b218b3ace21976c39bdf8cbb
lvch0 <100215416+lvch0@users.noreply.github.com>
2022-10-15 18:24:45 -0300

    qcyo

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -1,12 +1,102 @@
-// const mysql2 = require("mysql")
-// const util = require("util")
+const db = require("../../config/db.config")
+const formCtrl = {}
 
-// const addForm = 
-//     async (req, res) => {
-//         try {
-//             let query = 'INSERT INTO '
-//         } catch (e) {
-//             console.error(e.message)
-//             res.status(413).send({"mensaje": e.message})
-//         }
-//     }
+formCtrl.getForms = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+formCtrl.getForm = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+formCtrl.createForm = async (req, res) => {
+    try {
+        const {
+            nombreDenunciante, apellDenunciante, dniDenunciante, //!tabla denunciante//!
+            calleDomiDenunciante, numeDomiDenunciante, calleRefeUnoDomiDenunciante,  //!tabla domiciliodenunciante//!   
+            calleRefeDosDomiDenunciante, chacraDomiDenunciante,
+            nombreBarrio,  //!tabla barrio//!
+            idDirec,  //!tabla direcciones//!
+            idAsunDenun,  //!tabla asuntoDenuncia//!
+            descripProceDenun, //!tabla procedenciaDenuncia//!
+            calleUbiProble, //!tabla ubicacionProblematica//!
+            numeUbiProble,
+            edificioUbiProble,
+            pisoUbiProble,
+            descripUbiProble,
+            nombreDenunciado,  //!tabla denunciado//!
+            apellDenunciado,
+            apodoDenunciado,
+            descripUbiDenunciado,
+            descripDenunciado,
+            descripEstadoDenun,  //!tabla estadoDenuncia//!
+            descripAsunEspeci //!tabla asuntoEspecifico
+        } = req.body
+
+        const result = await db.denuncia.create({
+            data: {
+                denunciante: {
+                    create: {
+                        nombre: nombreDenunciante,
+                        apellido: apellDenunciante,
+                        dni: dniDenunciante,
+                        domiciliodenunciante: {
+                            create: {
+                                calle: calleDomiDenunciante,
+                                numeracion: numeDomiDenunciante,
+                                entreCalleUno: calleRefeUnoDomiDenunciante,
+                                entreCalleDos: calleRefeDosDomiDenunciante,
+                                chacra: chacraDomiDenunciante,
+                            }
+                        },
+                        barrio: {
+                            create: {
+                                nombre: nombreBarrio
+                            }
+                        }
+                    }
+
+                },
+                direcciones: {
+                    connect: {
+                        idDireccion: Number(idDirec)
+                    }
+                },
+                asuntodenuncia: {
+                    connect: {
+                        idAsuntoDenuncia: Number(idAsunDenun)
+                    },
+
+
+                },
+
+            }
+        })
+    } catch (error) {
+
+    }
+}
+
+formCtrl.updateForm = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
+
+formCtrl.deleteForm = async (req, res) => {
+    try {
+
+    } catch (error) {
+
+    }
+}
